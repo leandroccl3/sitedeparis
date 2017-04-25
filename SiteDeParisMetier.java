b6467620d0932217988549c66114f85bb0ecf383
leandroccl3 <leandroccl3@hotmail.com>
2017-04-25 16:14:46 +0200

    Update SiteDeParisMetier.java
    Cambie apellido por cristobal

--- SiteDeParisMetier.java
+++ SiteDeParisMetier.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 
 /**
  * 
- * @author Bernard Prou et Julien Mallet lucas borra
+ * @author Bernard Prou et Julien Mallet lucas cristobal
  * <br><br>
  * La classe qui contient toutes les méthodes "Métier" de la gestion du site de paris. 
  * <br><br>
