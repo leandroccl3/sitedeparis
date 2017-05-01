05d908cfb88297e52242bad75570bc9ebff8143b
leandroccl3 <leandroccl3@hotmail.com>
2017-05-01 18:23:19 +0200

    Create SiteDeParisMetier_try_catch.java
    Hay que modificar este fichero con los try catch.

---
+++ SiteDeParisMetier_try_catch.java
@@ -0,0 +1,450 @@
+package siteParis;
+
+import java.util.LinkedList;
+
+
+/**
+ * 
+ * @author Bernard Prou et Julien Mallet lucas perez
+ * <br><br>
+ * La classe qui contient toutes les méthodes "Métier" de la gestion du site de paris. 
+ * <br><br>
+ * Dans toutes les méthodes :
+ * <ul>
+ * <li>un paramètre de type <code>String</code> est invalide si il n'est pas instancié.</li>
+ *  <li>pour la validité d'un password de gestionnaire et d'un password de joueur :
+ * <ul>
+ * <li>       lettres et chiffres sont les seuls caractères autorisés </li>
+ * <li>       il doit avoir une longueur d'au moins 8 caractères </li>
+ * </ul></li>       
+ * <li>pour la validité d'un pseudo de joueur  :
+ * <ul>
+ * <li>        lettres et chiffres sont les seuls caractères autorisés  </li>
+ * <li>       il doit avoir une longueur d'au moins 4 caractères</li>
+ * </ul></li>       
+ * <li>pour la validité d'un prénom de joueur et d'un nom de joueur :
+ *  <ul>
+ *  <li>       lettres et tiret sont les seuls caractères autorisés  </li>
+ *  <li>      il  doit avoir une longueur d'au moins 1 caractère </li>
+ * </ul></li>
+ * <li>pour la validité d'une compétition  :       
+ *  <ul>
+ *  <li>       lettres, chiffres, point, trait d'union et souligné sont les seuls caractères autorisés </li>
+ *  <li>      elle  doit avoir une longueur d'au moins 4 caractères</li>
+ * </ul></li>       
+ * <li>pour la validité d'un compétiteur  :       
+ *  <ul>
+ *  <li>       lettres, chiffres, trait d'union et souligné sont les seuls caractères autorisés </li>
+ *  <li>      il doit avoir une longueur d'au moins 4 caractères.</li>
+ * </ul></li></ul>
+ */
+
+public class SiteDeParisMetier {
+
+linkedList <String> listaNom = new linkedList();
+     listaNom.add("acosta"); 
+     listaNom.add("rodriguez");
+     listaNom.add("cristobal");
+     listaNom.add("borracci");   
+     
+     
+linkedList <String> listaPrenom = new linkedList();
+     listaNom.add("pepe"); 
+     listaNom.add("coco");
+     listaNom.add("toto");
+     listaNom.add("ramon");   
+     
+     
+     
+linkedList <String> listaPseudo = new linkedList();
+        listaNom.add("pepito"); 
+        listaNom.add("co");
+        listaNom.add("to");
+        listaNom.add("ramono");
+     
+     
+linkedList <String> listaPass = new linkedList();
+        listaPass.add("1234");     
+
+	
+linkedList <String> listaComp = new linkedList(),
+	listaComp.add("basquet");
+	listaComp.add("tennis");
+	listaComp.add("fútbol");
+	listaComp.add("rugby");
+	
+	
+	/**
+	 * constructeur de <code>SiteDeParisMetier</code>. 
+	 * 
+	 * @param passwordGestionnaire   le password du gestionnaire.   
+	 * 
+	 * @throws MetierException  levée 
+	 * si le <code>passwordGestionnaire</code>  est invalide 
+	 */
+public SiteDeParisMetier(String passwordGestionnaire) throws MetierException{
+	if (passwordGestionnaire==null) || (passwordGestionnaire.length()<8){
+		throw new MetierException("Password Gestionaire no debe ser nulo ni menor a 8 caracteres");
+	}
+}
+		
+//Lanza una excepcion si el password del gestionario tiene una longitud menor a 8 caracteres. 
+         
+public SiteDeParisMetier(String passwordJoueur) throws MetierException {
+	if (passwordJoueur==null) || (passwordJoueur.length()<8){
+		throw new MetierException("El Password Joueur no debe ser nulo ni menor a 8 caracteres"); 
+	}
+}
+
+//Lanza una excepcion si el password del jugador tiene una longitud menor a 8 caracteres. 
+
+public SiteDeParisMetier(String nomJoueur) throws MetierException {
+	if (nomJoueur.lenght()<=4){
+		throw new MetierException("El nombre del jugador no debe ser ni menor a 4 caracteres"); 
+	}
+}
+	
+//Lanza una excepcion si el nombre del jugador tiene una longitud menor a 4 caracteres. 
+
+public SiteDeParisMetier(String nomJoueur) throws MetierException {
+	if (not((nomJoueur)evt.getKeyChar()>=65 & (nomJoueur)evt.getKeyChar()<=90) & (nomJoueur)evt.getKeyChar()>=97 & (nomJoueur)evt.getKeyChar()<=122) && (nomJoueur)evt.getKeyChar()=45))){
+		throw new MetierException("caracter erroneo(caracteres autorizados [a-z,A-Z,-]");
+	}
+}
+
+//Lanza una excepcion si el password del gestionario tiene caracteres diferentes a [a-z,A-Z,-]. 	
+	
+public SiteDeParisMetier(String nomCompetition) throws MetierException {
+	if (not(((nomCompetition)evt.getKeyChar()>=65 && (nomCompetition)evt.getKeyChar()<=90)) || ((nomCompetition)evt.getKeyChar()>=97 && (nomCompetition)evt.getKeyChar()<=122)) && ((nomCompetition)evt.getKeyChar()>=48 || (nomCompetition)evt.getKeyChar()<=57)) || (nomCompetition)evt.getKeyChar()=45) || (nomCompetition)evt.getKeyChar()=46) || (nomCompetition)evt.getKeyChar()=95))){
+		throw new MetierException("caracter erroneo(caracteres autorizados [a-z,A-Z,0-9,.,_,-,~]");
+	}
+}
+	
+
+//Lanza una excepcion si el password del gestionario tiene caracteres diferentes a [a-z,A-Z,0-9,.,_,-,~]. 	
+
+public SiteDeParisMetier(String passwordJoueur) throws MetierException {
+	if (not(((nomCompetition)evt.getKeyChar()>=48 && (nomCompetition)evt.getKeyChar()<=57)) || ((nomCompetition)evt.getKeyChar()>=65 && (nomCompetition)evt.getKeyChar()<=90)) || ((nomCompetition)evt.getKeyChar()>=97 && (nomCompetition)evt.getKeyChar()<=122)))){
+		throw new MetierException("caracter erroneo (Los caracteres autorizados son [a-z,A-Z,0-9]");
+	}
+}
+   
+
+//Lanza una excepcion si el password del gestionario tiene caracteres diferentes a [a-z,A-Z,0-9]
+
+
+
+
+	// Les méthodes du gestionnaire (avec mot de passe gestionnaire)
+
+
+
+	/**
+	 * inscrire un joueur. 
+	 * 
+	 * @param nom   le nom du joueur 
+	 * @param prenom   le prénom du joueur   
+	 * @param pseudo   le pseudo du joueur  
+	 * @param passwordGestionnaire  le password du gestionnaire du site  
+	 * 
+	 * @throws MetierException   levée  
+	 * si le <code>passwordGestionnaire</code> proposé est invalide,
+	 * si le <code>passwordGestionnaire</code> est incorrect.
+* @throws JoueurExistantException   levée si un joueur existe avec les mêmes noms et prénoms ou le même pseudo.
+	 * @throws JoueurException levée si <code>nom</code>, <code>prenom</code>, <code>pseudo</code> sont invalides.
+	 * 
+	 * @return le mot de passe (déterminé par le site) du nouveau joueur inscrit.
+	 */
+public String inscrireJoueur(String nom, String prenom, String pseudo, String passwordGestionnaire) throws MetierException, JoueurExistantException, JoueurException {
+	return "unPasswordUnique";
+}
+ 
+	   
+  
+public SiteDeParisMetier(String nomJoueur, String prenomJoueur) throws JoueurExistantException {
+	for (i=0; i < listaNom.size(); i++ ){ 
+		if (nomJoueur == listaNom(i)){
+			if (prenomJoueur == listaPrenom(i)){
+				throw new MetierException("ya existe un jugador con ese Nombre y Apellido ");}
+		}
+	}
+}
+	    
+//Esta excepcion se ejecuta si comparando elemento a elemento de la lista de 
+//nombres y apellidos, la persona a inscribirse posee el mismo nombre y el mismo apellido
+
+public SiteDeParisMetier(String pseudoJoueur) throws JoueurExistantException {
+	for (i=0; i < listaPseudo.size(); i++ ){ 
+		if (pseudoJoueur == listaPseudo(i)){
+			throw new MetierException("ya existe un jugador con ese pseudonimo ");
+		}
+	}
+}
+
+//Esta excepcion se ejecuta si comparando elemento a elemento de la lista de 
+//pseudonimos, la persona a inscribirse posee el mismo pesudonimo
+
+/**
+	 * supprimer un joueur. 
+	 * 
+	 * @param nom   le nom du joueur 
+	 * @param prenom   le prénom du joueur   
+	 * @param pseudo   le pseudo du joueur  
+	 * @param passwordGestionnaire  le password du gestionnaire du site  
+	 * 
+	 * @throws MetierException
+	 * si le <code>passwordGestionnaire</code>  est invalide,
+	 * si le <code>passwordGestionnaire</code> est incorrect.
+	 * @throws JoueurInexistantException   levée si il n'y a pas de joueur  avec le même <code>nom</code>, <code>prenom</code>  et <code>pseudo</code>.
+	 * @throws JoueurException levée 
+	 * si le joueur a des paris en cours,
+	 * si <code>nom</code>, <code>prenom</code>, <code>pseudo</code> sont invalides.
+	 * 
+	 * @return le nombre de jetons à rembourser  au joueur qui vient d'être désinscrit.
+	 * 
+	 */
+public long desinscrireJoueur(String nom, String prenom, String pseudo, String passwordGestionnaire) throws MetierException, JoueurInexistantException, JoueurException {
+	return 0;
+}
+
+
+  
+public SiteDeParisMetier(String passwordGestionnaire) throws MetierException {
+	for (i=1; i < listaPass.size(); i++ ){ 
+		if (passwordGestionnaire == listaPass(i)){
+			throw new MetierException("password invalido ");
+		}
+	}
+}
+  
+public SiteDeParisMetier(String pseudoJoueur,String nomJoueur, String PrenomJoueur) throws MetierException {
+	for (i=1; i < listaPseudo.size(); i++ ){
+		if (pseudoJoueur =! listaPseudo(i)){
+			throw new MetierException("datos invalidos");
+		}
+		else if (nomJoueur =! listaNom(i)){
+			throw new MetierException("datos invalidos ");
+		}
+		else if (prenomJoueur =! listaPrenom(i)){
+			throw new MetierException("datos invalidos  ");
+		}
+	}
+}
+     
+     
+     
+
+
+	/**
+	 * ajouter une compétition.  
+	 * 
+	 * @param competition le nom de la compétition
+	 * @param dateCloture   la date à partir de laquelle il ne sera plus possible de miser  
+	 * @param competiteurs   les noms des différents compétiteurs de la compétition 
+	 * @param passwordGestionnaire  le password du gestionnaire du site 
+	 * 
+	 * @throws MetierException levée si le tableau des
+	 * compétiteurs n'est pas instancié, si le
+	 * <code>passwordGestionnaire</code> est invalide, si le------------------
+	 * <code>passwordGestionnaire</code> est incorrect.
+	 * @throws CompetitionExistanteException levée si une compétition existe avec le même nom. 
+	 * @throws CompetitionException levée si le nom de la
+	 * compétition ou des compétiteurs sont invalides, si il y a
+	 * moins de 2 compétiteurs, si un des competiteurs n'est pas instancié,
+	 * si deux compétiteurs ont le même nom, si la date de clôture 
+	 * n'est pas instanciée ou est dépassée.
+	 */
+public void ajouterCompetition(String competition, DateFrancaise dateCloture, String [] competiteurs, String passwordGestionnaire) throws MetierException, CompetitionExistanteException, CompetitionException  {
+}
+	   
+public SiteDeParisMetier (String competition) throws CompetitionExistanteException {
+	for (i=0, i<listaComp,i++){
+		if (competition == listaComp[i]) {
+		//continuar
+		}
+	}
+}
+	   
+	   
+
+
+	/**
+	 * solder une compétition vainqueur (compétition avec vainqueur).  
+	 * 
+	 * Chaque joueur ayant misé sur cette compétition
+	 * en choisissant ce compétiteur est crédité d'un nombre de
+	 * jetons égal à :
+	 * 
+	 * (le montant de sa mise * la somme des
+	 * jetons misés pour cette compétition) / la somme des jetons
+	 * misés sur ce compétiteur.
+	 *
+	 * Si aucun joueur n'a trouvé le
+	 * bon compétiteur, des jetons sont crédités aux joueurs ayant
+	 * misé sur cette compétition (conformément au montant de
+	 * leurs mises). La compétition est "supprimée" si il ne reste
+	 * plus de mises suite à ce solde.
+	 * 
+	 * @param competition   le nom de la compétition  
+	 * @param vainqueur   le nom du vainqueur de la compétition 
+	 * @param passwordGestionnaire  le password du gestionnaire du site 
+	 * 
+	 * @throws MetierException   levée 
+	 * si le <code>passwordGestionnaire</code>  est invalide,
+	 * si le <code>passwordGestionnaire</code> est incorrect.
+	 * @throws CompetitionInexistanteException   levée si il n'existe pas de compétition de même nom.
+	 * @throws CompetitionException levée 
+	 * si le nom de la compétition ou du vainqueur est invalide, 
+	 * si il n'existe pas de compétiteur du nom du vainqueur dans la compétition,
+	 * si la date de clôture de la compétition est dans le futur.
+	 * 
+	 */	
+public void solderVainqueur(String competition, String vainqueur, String passwordGestionnaire) throws MetierException,  CompetitionInexistanteException, CompetitionException  {
+}
+
+
+
+	/**
+	 * créditer le compte en jetons d'un joueur du site de paris.
+	 * 
+	 * @param nom   le nom du joueur 
+	 * @param prenom   le prénom du joueur   
+	 * @param pseudo   le pseudo du joueur  
+	 * @param sommeEnJetons   la somme en jetons à créditer  
+	 * @param passwordGestionnaire  le password du gestionnaire du site  
+	 * 
+	 * @throws MetierException   levée 
+	 * si le <code>passwordGestionnaire</code>  est invalide,
+	 * si le <code>passwordGestionnaire</code> est incorrect,
+	 * si la somme en jetons est négative.
+	 * @throws JoueurException levée  
+	 * si <code>nom</code>, <code>prenom</code>,  <code>pseudo</code> sont invalides.
+	 * @throws JoueurInexistantException   levée si il n'y a pas de joueur  avec les mêmes nom,  prénom et pseudo.
+	 */
+	public void crediterJoueur(String nom, String prenom, String pseudo, long sommeEnJetons, String passwordGestionnaire) throws MetierException, JoueurException, JoueurInexistantException {
+
+	}
+
+
+	/**
+	 * débiter le compte en jetons d'un joueur du site de paris.
+	 * 
+	 * @param nom   le nom du joueur 
+	 * @param prenom   le prénom du joueur   
+	 * @param pseudo   le pseudo du joueur  
+	 * @param sommeEnJetons   la somme en jetons à débiter  
+	 * @param passwordGestionnaire  le password du gestionnaire du site  
+	 * 
+	 * @throws MetierException   levée 
+	 * si le <code>passwordGestionnaire</code>  est invalide,
+	 * si le <code>passwordGestionnaire</code> est incorrect,
+	 * si la somme en jetons est négative.
+	 * @throws JoueurException levée  
+	 * si <code>nom</code>, <code>prenom</code>,  <code>pseudo</code> sont invalides 
+	 * si le compte en jetons du joueur est insuffisant (il deviendrait négatif).
+	 * @throws JoueurInexistantException   levée si il n'y a pas de joueur  avec les mêmes nom,  prénom et pseudo.
+	 * 
+	 */
+
+public void debiterJoueur(String nom, String prenom, String pseudo, long sommeEnJetons, String passwordGestionnaire) throws  MetierException, JoueurInexistantException, JoueurException {
+}
+
+
+	/** 
+	 * consulter les  joueurs.
+	 * 
+	 * @param passwordGestionnaire  le password du gestionnaire du site de paris 
+	 * @throws MetierException   levée  
+	 * si le <code>passwordGestionnaire</code>  est invalide,
+	 * si le <code>passwordGestionnaire</code> est incorrect.
+	 * 
+	 * @return une liste de liste dont les éléments (de type <code>String</code>) représentent un joueur avec dans l'ordre : 
+	 *  <ul>
+	 *  <li>       le nom du joueur  </li>
+	 *  <li>       le prénom du joueur </li>
+	 *  <li>       le pseudo du joueur  </li>
+	 *  <li>       son compte en jetons restant disponibles </li>
+	 *  <li>       le total de jetons engagés dans ses mises en cours. </li>
+	 *  </ul>
+	 */
+public LinkedList <LinkedList <String>> consulterJoueurs(String passwordGestionnaire) throws MetierException {
+	return new LinkedList <LinkedList <String>>();
+}
+
+
+	// Les méthodes avec mot de passe utilisateur
+
+
+
+	/**
+	 * miserVainqueur  (parier sur une compétition, en désignant un vainqueur).
+	 * Le compte du joueur est débité du nombre de jetons misés.
+	 * 
+	 * @param pseudo   le pseudo du joueur  
+	 * @param passwordJoueur  le password du joueur  
+	 * @param miseEnJetons   la somme en jetons à miser  
+	 * @param competition   le nom de la compétition  relative au pari effectué
+	 * @param vainqueurEnvisage   le nom du compétiteur  sur lequel le joueur mise comme étant le  vainqueur de la compétition  
+	 * 
+	 * @throws MetierException levée si la somme en jetons est négative.
+	 * @throws JoueurInexistantException levée si il n'y a pas de
+	 * joueur avec les mêmes pseudos et password.
+	 * @throws CompetitionInexistanteException   levée si il n'existe pas de compétition de même nom. 
+	 * @throws CompetitionException levée 
+	 * si <code>competition</code> ou <code>vainqueurEnvisage</code> sont invalides,
+	 * si il n'existe pas un compétiteur de  nom <code>vainqueurEnvisage</code> dans la compétition,
+	 * si la compétition n'est plus ouverte (la date de clôture est dans le passé).
+	 * @throws JoueurException   levée 
+	 * si <code>pseudo</code> ou <code>password</code> sont invalides, 
+	 * si le <code>compteEnJetons</code> du joueur est insuffisant (il deviendrait négatif).
+	 */
+public void miserVainqueur(String pseudo, String passwordJoueur, long miseEnJetons, String competition, String vainqueurEnvisage) throws MetierException, JoueurInexistantException, CompetitionInexistanteException, CompetitionException, JoueurException  {
+}
+
+
+    
+
+	// Les méthodes sans mot de passe
+
+
+	/**
+	 * connaître les compétitions en cours.
+	 * 
+	 * @return une liste de liste dont les éléments (de type <code>String</code>) représentent une compétition avec dans l'ordre : 
+	 *  <ul>
+	 *  <li>       le nom de la compétition,  </li>
+	 *  <li>       la date de clôture de la compétition. </li>
+	 *  </ul>
+	 */
+public LinkedList <LinkedList <String>> consulterCompetitions(){
+	return new LinkedList <LinkedList <String>>();
+} 
+
+	/**
+	 * connaître  la liste des noms des compétiteurs d'une compétition.  
+	 * 
+	 * @param competition   le nom de la compétition  
+	 * 
+	 * @throws CompetitionException   levée  
+	 * si le nom de la compétition est invalide.
+	 * @throws CompetitionInexistanteException   levée si il n'existe pas de compétition de même nom. 
+	 * 
+	 * @return la liste des compétiteurs de la  compétition.
+	 */
+public LinkedList <String> consulterCompetiteurs(String competition) throws CompetitionException, CompetitionInexistanteException{
+	return new LinkedList <String> ();
+}
+
+	/**
+	 * vérifier la validité du password du gestionnaire.
+	 * 
+	 * @param passwordGestionnaire   le password du gestionnaire à vérifier
+	 * 
+	 * @throws MetierException   levée 
+	 * si le <code>passwordGestionnaire</code> est invalide.  
+	 */
+protected void validitePasswordGestionnaire(String passwordGestionnaire) throws MetierException {
+	if (passwordGestionnaire==null) throw new MetierException();{
+	}
+	if (!passwordGestionnaire.matches("[0-9A-Za-z]{8,}")) throw new MetierException();{}
+}
